46c0770c9bae31bcab9014a9a95f7327c0954034
lyskovichigor <lyskovichigor@gmail.com>
2022-06-30 20:18:11 +0300

    #замена пробелов

--- 3.1.py
+++ 3.1.py
@@ -3,3 +3,9 @@ family = "лыскович"
 name = "игорь"
 name_2 = "николаевич"
 print(family, name, name_2, sep='-')
+
+
+color_1: str = "ellow"
+color_2: str = "black"
+colorit: str = color_1 + "-" + color_2
+print(colorit)
